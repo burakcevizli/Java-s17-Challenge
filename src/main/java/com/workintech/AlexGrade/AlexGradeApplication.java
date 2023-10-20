dfe0c6cdae1982a9e586d07322d061d05f6f15cf
KrieqqeR <burakcevizli@gmail.com>
2023-10-20 13:50:35 +0300

    gorev 1 - 3 den devam...

---
+++ src/main/java/com/workintech/AlexGrade/AlexGradeApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.AlexGrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexGradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexGradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/AlexGrade/AlexGradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.AlexGrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexGradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
